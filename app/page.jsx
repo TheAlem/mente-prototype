ffd36dd1460e568e015f0330079b3531dd370d3b
Jose Alem Rodriguez Valverde <118365851+TheAlem@users.noreply.github.com>
2026-09-09 23:17:26 -0400

    next.js a vite

--- app/page.jsx
+++
@@ -1,5 +0,0 @@
-import MentePrototype from "../components/MentePrototype";
-
-export default function Home() {
-  return <MentePrototype />;
-}
--- components/MentePrototype.jsx
+++ components/MentePrototype.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState, useMemo } from "react";
 import {
   Menu, X, ChevronRight, ChevronLeft, MapPin, Clock, Phone, DollarSign,
